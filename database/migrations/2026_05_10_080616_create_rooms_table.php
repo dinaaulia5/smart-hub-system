0800d492d45cbde95bfc665424fb4bcbc056b266
dina.na <dina.na@bki.co.id>
2026-05-15 09:02:27 +0700

    feat: implement core database migrations

---
+++ database/migrations/2026_05_10_080616_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->smallInteger('capacity');
+            $table->string('location');
+            $table->enum('status', ['available', 'maintenance'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
